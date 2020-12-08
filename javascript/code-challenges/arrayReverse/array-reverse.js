93c40fb609b427dc574ba904538c009c30f2b454
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-08 10:31:34 -0800

    Adjusted code for challenge 01 to not use a native method.

--- javascript/code-challenges/arrayReverse/array-reverse.js
+++ javascript/code-challenges/arrayReverse/array-reverse.js
@@ -1,12 +1,27 @@
+'use strict';
+
 let array = [1, 2, 3, 4, 5];
 
 function reverseArray(arr) {
   let reversedArray = [];
-  for (let i=arr.length-1; i>=0; i--) {
-    reversedArray.push(arr[i]);
-  }
-  console.log(reversedArray);
-  return reversedArray;
-}
+  let answerIndex = 0;
+
+  for (let i = arr.length-1; i >= 0; i--) {
+        reversedArray[answerIndex] = arr[i];
+        answerIndex++;
+      }
+      console.log(reversedArray);
+    }
 
 reverseArray(array);
+
+// function reverseArray(arr) {
+//   let reversedArray = [];
+//   for (let i=arr.length-1; i>=0; i--) {
+//     reversedArray.push(arr[i]);
+//   }
+//   console.log(reversedArray);
+//   return reversedArray;
+// }
+
+// reverseArray(array);
