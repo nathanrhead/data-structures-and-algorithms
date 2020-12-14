df0c30092ba61a5ddc86c6d103d9401611d21f19
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-13 17:02:16 -0800

    Add features and 3 of 5 tests.

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,29 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../linked-list');
 
 describe('Linked List', () => {
   it('works', () => {
+    new LinkedList(true);
     expect(true).toBeTruthy();
   });
-});
+
+  it('inserts a node at the begining of an empty list', () => {
+    const list = new LinkedList();
+    list.insert('Henry');
+    expect(list.head.value).toEqual('Henry');
+  })
+
+  it('inserts a node at the beginning of a list', () => {
+    const list = new LinkedList();
+    list.insert('Norah');
+    expect(list.head.value).toEqual('Norah');
+  })
+
+  // it('looks for the presence of a given value in the list', () => {
+  //   const list = new LinkedList();
+
+  // })
+
+})
---
+++ javascript/linked-list/linked-list.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Node = require('./node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+// Insert a node at the head.
+  insert(value) { 
+    if(this.head === null) { 
+      const node = new Node(value);
+      this.head = node;
+    } else {
+      const node = new Node(value);
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
+// Insert a node at the end of a list.
+  append(value) {
+    const node = new Node(value);
+    let currentNode = this.head;
+    while(currentNode.next !== null) {
+      currentNode = currentNode.next;
+    };
+    currentNode.next = node;
+  }
+
+// Look for the presence of a given value in the list.
+  includes(value) {
+    let currentNode = this.head;
+    while(currentNode.next !== value || currentNode.next !== null) {
+      currentNode = currentNode.next;
+    };
+    return currentNode.value === value ? true : false;
+  }
+
+// Create a string of all the linked list's values from a to the nth.
+  toString() {
+    let currentNode = this.head;
+    let allValues = '';
+    while(currentNode.next !== null) {
+      allValues = '{ currentNode } -> ';
+    }
+    return allValues;
+  }
+
+}
+
+module.exports = LinkedList;
