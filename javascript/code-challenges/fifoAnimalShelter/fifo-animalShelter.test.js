f175e5843b02367165995bd0a5654c8618af4590
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-22 15:58:27 -0800

    Complete code challenge, with whiteboard, code, and tests.

---
+++ javascript/code-challenges/fifoAnimalShelter/fifo-animalShelter.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Fifo = require('./fifo-animal-shelter.js');
+
+describe('enqueue', () => {
+  it ('adds a dog to the dogQueue', () => {
+    let newAnimal = new Fifo();
+    newAnimal.enqueue('dog');
+    expect(newAnimal.dogQueue.front.value).toEqual('dog');
+  });
+
+  it ('adds a cat to the catQueue', () => {
+    let newAnimal = new Fifo();
+    newAnimal.enqueue('cat');
+    expect(newAnimal.catQueue.front.value).toEqual('cat');
+  });
+
+  it ('throws an exception when given an invalid input', () => {
+    let newAnimal = new Fifo();
+    expect(() => newAnimal.enqueue('fish')).toThrow('Invalid entry.');
+  })
+});
+
+describe('dequeue', () => {
+  it('removes a dog from the dogQueue', () => {
+    let newAnimal = new Fifo()
+    newAnimal.enqueue('dog');
+    newAnimal.dequeue('dog');
+    expect(newAnimal.dogQueue.front).toEqual(null);
+  });
+  it('removes a dog from the catQueue', () => {
+    let newAnimal = new Fifo()
+    newAnimal.enqueue('cat');
+    newAnimal.dequeue('cat');
+    expect(newAnimal.catQueue.front).toEqual(null);
+  });
+
+  it('returns null when given an invalid input', () => {
+    let newAnimal = new Fifo();
+    newAnimal.enqueue('cat');
+    expect(newAnimal.dequeue('fish')).toBe(null);
+  });
+});
