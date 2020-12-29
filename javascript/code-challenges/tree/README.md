498b8287a9c0b58d52d477be4196c50a75d02159
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-28 17:54:48 -0800

    Complete code challenge 16 and 15 with tests.

--- javascript/code-challenges/tree/README.md
+++ javascript/code-challenges/tree/README.md
@@ -1,7 +1,6 @@
-# Binary Tree & BST Implementation
+# Binary Tree & BST Implementation: Challenges 15 & 16
 
-- This is the code challenge for class 15 of Code Fellows 401.
-- The task is multitude:
+- Code challenge 15:
   - to create a Node class that has properties for the value stored in the node, the left child node, and the right child node;
   - to create a BinaryTree class;
   - to define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately;
@@ -10,6 +9,8 @@
   - to define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
   -to define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
 
+- Code Challenge 16: extend the binary tree class with a method that returns the maximum value of the tree.
+
 ## Challenge
 
 - Write the code and tests for it.
@@ -17,10 +18,20 @@
 
 ## Approach & Efficiency
 
+### Challenge 15
 - Big O for space: not yet known.
-- Big 0 for time: not yet known.
+- Big O for time: not yet known.
+
+### Challenge 16
+- Big O for space: O(1): a variable maxTree is created.
+- Big O for time: O(n): the preOrder method has a height of log n, so time is O(h).
 
 ## Solution
 
+### Challenge 15
 - No whiteboard required.
-[Coded Solution](./tree.js): no solution yet found.
+[Code 15 Solution](./tree.js): no solution yet found.
+
+### Challenge 16
+![Code 16 whiteboard](../assets/code-challenge16.png);
+[Code 15 Solution](./tree.js).
--- javascript/code-challenges/tree/tree.js
+++ javascript/code-challenges/tree/tree.js
@@ -4,50 +4,71 @@
 class Node {
   constructor(value = 0, left = null, right = null) { 
     this.value = value;
-    this.right = right;
     this.left = left;
-  }
+    this.right = right;
+  };
 }
 
-// Create a BinaryTree class
+// Create a BinaryTree class and a BinarySearchTreeClass (which the add method will do).
 class BinaryTree {
   constructor() {
     this.root = null;
   };
+
+  // Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree: this one will make a BST and may only accept integers.
+  add(value) {
+    if(typeof value !== 'number') {
+      return null;
+    }
+
+    if(!this.root) {
+      this.root = new Node(value);
+      return;
+    }
+
+    let _insert = (node) => {
+      if(value < node.value) {
+        if(node.left === null) {
+          node.left = new Node(value);
+          return;
+        } else if (node.left !== null) {
+          return +_insert(node.left);
+        }
+      } else if(value >= node.value) {
+        if(node.right === null) {
+          node.right = new Node(value);
+          return;
+        } else if (node.right !== null) {
+          return _insert(node.right);
+        }
+      }
+    }
+    _insert(this.root);
+  }
 
   // Define a method for each of the depth-first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
 
   preOrder() { // depth-first search; works with BT and BST
     // Create an array that will hold the values
     const results = [];
-
     let _walk = (node) => {
       results.push(node.value);
       if (node.left) _walk(node.left);
       if (node.right) _walk(node.right);
     }
     _walk(this.root);
-
-    // let node = new Node(value);
-    // if(!this.root) { 
-    //   this.root = node;
-    // } else { 
-    //   let temp = this.root;
-    //   this.root = node; 
-    //   this.temp = left;
-    // }
+    return results;
   };
 
   inOrder() {
-
     const results = [];
-
     let _walk = (node) => {
       if (node.left) _walk(node.left);
       results.push(node.value);
       if (node.right) _walk(node.right);
     }
     _walk(this.root);
+    return results;
   };
 
   postOrder() {
@@ -59,58 +80,57 @@ class BinaryTree {
       results.push(node.value);
     }
     _walk(this.root);
+    return results;
   };
 
-  add(value) {
-    if (typeof value !== 'number') {
-      return null; // or throw an error
-    }
-    // Make a new node
-    if (!this.root) {
-    this.root = new Node(value);
-    return;
-    }
-    let _insert = (node) => { // to insert a leaf
-      //if the value is less than that of the root
-      if (value < node.value) {
-        if (node.left === null) {
-          node.left = new Node(value);
-        }
-      } else if (node.left !== null) {
-        return _insert(node.left);
-         // if the value is greater than that of the root
-      } else if (value >= node.value) { // The equals is arbitrary: we could have sent equal values left or forbidden them altogether
-        if (node.right === null) {
-          node.right = new Node(value);
-          return;
-        } else if (node.right !== null) {
-          return _insert(node.right); // Recursion: make node.right the beginning (root) of the _insert function.
-        }
-      }
-  } 
-  _insert(this.root);
-
-}
-
-
-// Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
-
-// Create a BinarySearchTree class
-class BinarySearchTree {
-  constructor() {
-
-  };
-
-// Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
-  add(value) {
-
-  };
 
   // Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
   contains(value) {
-
+    if(!this.root) return null;
+    let boolean = false;
+    let _walk = (node) => {
+      if(node.value === value) { boolean = true };
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+      console.log('Node.Value:', node.value, 'Value:', value);
+    }
+    _walk(this.root);
+    return boolean;
   };
+
+  // Find the Maximum Value in a Binary Tree (code challenge 16 of CF 401).
+  findMax() {
+    let treeMax = 0;
+    if(!this.root) return null;
+    
+    let _walk = (node) => {
+      if(node.value > treeMax) { treeMax = node.value};
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+    };
+    _walk(this.root);
+    return treeMax;
+  }
+  
+// Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
 
 }
 
+module.exports = BinaryTree;
 
+
+// ///////Tests//////////
+
+// let binaryTree = new BinaryTree();
+// binaryTree.add(8);
+// binaryTree.add(2);
+// binaryTree.add(7);
+// binaryTree.add(4);
+// binaryTree.add(6);
+// binaryTree.add(5);
+// binaryTree.add(3);
+// binaryTree.add(1);
+// binaryTree.add(9);
+// console.log('Binary Tree:', binaryTree);
+// binaryTree.preOrder();
+// console.log('Results:', binaryTree.preOrder());
